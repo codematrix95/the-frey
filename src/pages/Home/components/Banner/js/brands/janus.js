2b6b78b024c4588778da71605c4a9eb39ffe32a1
codematrix95 <codematrix95@gmail.com>
2025-05-25 11:41:35 -0500

    refactor-banner-arrays

---
+++ src/pages/Home/components/Banner/js/brands/janus.js
@@ -0,0 +1,13 @@
+const janus = {
+    name: 'Healthcare',
+    desktopImg:
+        'https://freywebpro.wpenginepowered.com/wp-content/uploads/2021/10/01_Home-1.jpg',
+    mobileImg:
+        'https://freywebpro.wpenginepowered.com/wp-content/uploads/2021/10/02_Home-3.jpg',
+    mobileMenuImg:
+        'https://freywebpro.wpenginepowered.com/wp-content/uploads/2021/10/04_Home-1.jpg',
+    shopModuleImg:
+        'https://freywebpro.wpenginepowered.com/wp-content/uploads/2021/10/03_Home-1.jpg',
+};
+
+export default janus;
---
+++ src/pages/Home/components/Banner/js/brands/spearFish.js
@@ -0,0 +1,13 @@
+const spearFish = {
+    name: 'CRE',
+    desktopImg:
+        'https://freywebpro.wpenginepowered.com/wp-content/uploads/2021/10/01_Home.jpg',
+    mobileImg:
+        'https://freywebpro.wpenginepowered.com/wp-content/uploads/2021/10/02_Home.jpg',
+    mobileMenuImg:
+        'https://freywebpro.wpenginepowered.com/wp-content/uploads/2021/10/04-Home.jpg',
+    shopModuleImg:
+        'https://freywebpro.wpenginepowered.com/wp-content/uploads/2021/10/03-Home-1.jpg',
+};
+
+export default spearFish;
