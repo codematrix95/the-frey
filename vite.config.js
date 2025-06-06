46d5136f97f494a748790bf1dafa48d5e8b93e98
codematrix95 <codematrix95@gmail.com>
2025-06-06 12:55:46 -0500

    add-path-alias-for-scss

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: 'localhost',
-  },
-  plugins: [react()],
-})
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@scss': path.resolve(__dirname, 'src/scss'), // 👈 your SCSS folder
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: '', // optional
+                includePaths: [path.resolve(__dirname, 'src/scss')],
+            },
+        },
+    },
+    server: {
+        host: 'localhost',
+    },
+    plugins: [react()],
+});
