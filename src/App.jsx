4e53be0b5f70c2d0366193b48d56de1cbc33d7e6
codematrix95 <codematrix95@gmail.com>
2025-06-11 11:04:43 -0500

    fix-nav-display-for-larger-devices

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import Home from './pages/Home/Home';
 import Header from './components/Header/Header';
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Routes>
-        <Route path="/the-frey-build/" element={<Home />} />
-      </Routes>
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <Routes>
+                <Route
+                    path="/the-frey-build/"
+                    element={<Home />}
+                />
+            </Routes>
+        </>
+    );
 }
 
-export default App
+export default App;
